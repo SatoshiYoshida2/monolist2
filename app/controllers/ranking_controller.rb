7b6293657420259f0c90baca62f7d1c291823ebb
SatoshiYoshida2 <rarufu-710@docomo.ne.jp>
2016-07-22 07:49:04 +0000

    fix reverse want/have

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,11 +1,13 @@
 class RankingController < ApplicationController
   def have
-    item_keys = Have.group(:item_id).order('count_all desc').limit(10).count.keys
-    @items = Item.where(id: item_keys).sort_by{|o| item_keys.index(o.id)}
+    # item_keys = Have.group(:item_id).order('count_all desc').limit(10).count.keys
+    # @items = Item.where(id: item_keys).sort_by{|o| item_keys.index(o.id)}
+    @items = Have.rank10_items
   end
 
   def want
-    item_keys = Want.group(:item_id).order('count_all desc').limit(10).count.keys
-    @items = Item.where(id: item_keys).sort_by{|o| item_keys.index(o.id)}
+    # item_keys = Want.group(:item_id).order('count_all desc').limit(10).count.keys
+    # @items = Item.where(id: item_keys).sort_by{|o| item_keys.index(o.id)}
+    @items = Want.rank10_items
   end
 end
--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -1,4 +1,10 @@
 class Ownership < ActiveRecord::Base
   belongs_to :user, class_name: "User"
   belongs_to :item, class_name: "Item"
+  
+  def self.rank10_items
+    result = group(:item_id).order('count_all desc').limit(10).count
+    item_keys = result.keys
+    Item.where(id: item_keys).sort_by{|o| item_keys.index(o.id)}
+  end
 end
